ce698a2a7d1fe932c653538f37539605aa3fbaa5
Morgana Ximenes <morgana.ximenes@yahoo.ca>
2019-06-09 08:57:47 -0400

    First iteration of project

---
+++ data.js
@@ -0,0 +1,23 @@
+const postData = [
+  `<div class="post card mx-auto mb-2">
+            <img class="card-img-top" alt="img3" src="assets/img3.jpg">
+            <div class="card-body">
+              <img alt="avatar1" src="assets/avatar1.jpg" class="avatar float-left mr-2">
+              <h5 class="card-title my-auto">Alice</h5>
+              <p class="card-text">Quisque tempus pellentesque sapien eu bibendum. Mauris tortor nisl, pellentesque vitae suscipit eu, aliquam sed ligula. Pellentesque eu nisi mollis, tempor libero et, rhoncus diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut sodales, nisl ut porta ullamcorper, ligula turpis gravida justo, non ultricies risus ipsum vel eros.</p>
+              <button class="hide btn btn-secondary btn-sm position-absolute">Hide</button>
+              <i class="like float-right far fa-heart"></i>
+            </div>
+          </div>`,
+  
+  `<div class="post card mx-auto mb-2">
+            <img class="card-img-top" alt="img4" src="assets/img4.jpg">
+            <div class="card-body">
+              <img alt="avatar1" src="assets/avatar2.jpg" class="avatar float-left mr-2">
+              <h5 class="card-title my-auto">Bob</h5>
+              <p class="card-text">Sed sapien libero, fringilla id ex consequat, semper tincidunt lectus. Vivamus gravida neque ac odio eleifend porttitor. Nulla facilisi. Integer elementum eros vel turpis consectetur, ut pharetra justo venenatis. Nam vestibulum tristique elementum. Sed molestie risus non accumsan hendrerit. Ut mi neque, auctor eu ligula vitae, fermentum tincidunt dolor. Sed ac tellus sed risus venenatis tempus. Cras lobortis leo quis elit pharetra commodo. Nulla ut est a arcu feugiat tempor.</p>
+              <button class="hide btn btn-secondary btn-sm position-absolute">Hide</button>
+              <i class="like float-right far fa-heart"></i>
+            </div>
+          </div>`
+];
